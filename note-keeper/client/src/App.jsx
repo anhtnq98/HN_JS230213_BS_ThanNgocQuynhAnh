5b4fe60f14f8428d214ee86159c3b35ec0a9c84a
anhtnq98 <kethanlinh.otaku@gmail.com>
2023-05-26 11:01:49 +0700

    add

--- note-keeper/client/src/App.jsx
+++ note-keeper/client/src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [inputValue, setInput] = useState("");
+  const [notes, setNotes] = useState("");
 
   const handleChange = (e) => {
     setInput(e.target.value);
   };
 
-  const handleClick = (e) => {
-    
+  const handleClick = () => {
+    fetch("http://localhost:5000/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ inputValue }),
+    });
   };
+
+  const renderData = () => {
+    fetch("http://localhost:5000/", { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => {
+        setNotes(data);
+      });
+  };
+
+  useEffect(() => {
+    renderData();
+  }, []);
 
   return (
     <div className="App">
@@ -33,22 +50,19 @@ function App() {
         </div>
       </div>
       <div className="render-container">
-        <div className="block">
-          <div className="text">Ahuhu</div>
-          <div className="delete-container">
-            <div className="delete">
-              <i class="fa-sharp fa-solid fa-trash"></i>
+        {notes.data?.map((note) => (
+          <>
+            <div className="block">
+              <div className="text">{note.Content}</div>
+              <div className="delete-container">
+                <div className="delete">
+                  <i class="fa-sharp fa-solid fa-trash"></i>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="block">
-          <div className="text">Điểm kém rồi</div>
-          <div className="delete-container">
-            <div className="delete">
-              <i class="fa-sharp fa-solid fa-trash"></i>
-            </div>
-          </div>
-        </div>
+          </>
+        ))}
+  
       </div>
     </div>
   );
